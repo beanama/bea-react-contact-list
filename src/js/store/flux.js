5b0b57a7fb7c6576e637ed25c6a3fb213ec3a9a6
beanama <129529299+beanama@users.noreply.github.com>
2023-06-26 19:13:59 +0000

    configuration ok

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -3,41 +3,25 @@ const getState = ({ getStore, getActions, setStore }) => {
 		store: {
 			demo: [
 				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
+					full_name: "Dave Bradley",
+					email: "dave@gmail.com",
+					agenda_slug: "my_super_agenda",
+					address:"47568 NW 34ST, 33434 FL, USA",
+					phone:"7864445566"
 				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
+				
 			]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+
 			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+				
+				fetch()
+					.then()
+					.then(data => setStore({ "foo": data.bar }))
+				
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			
 		}
 	};
 };
